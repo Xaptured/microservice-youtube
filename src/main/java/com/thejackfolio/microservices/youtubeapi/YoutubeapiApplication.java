5500b5d78f04ae42ec8a567ea1061577f5e170be
Jack <jk19011999@gmail.com>
2023-07-28 21:50:53 +0530

    Created basic structure for the project

---
+++ src/main/java/com/thejackfolio/microservices/youtubeapi/YoutubeapiApplication.java
@@ -0,0 +1,13 @@
+package com.thejackfolio.microservices.youtubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeapiApplication.class, args);
+	}
+
+}
