35854ba18cb8e766dd30d42d4849196dcf531453
Jack <jk19011999@gmail.com>
2023-09-02 00:36:03 +0530

    Added necessary changes

--- src/main/java/com/thejackfolio/microservices/youtubeapi/controllers/YouTubeController.java
+++ src/main/java/com/thejackfolio/microservices/youtubeapi/controllers/YouTubeController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,7 @@ public class YouTubeController {
     )
     @GetMapping("/get-videos")
     @Retry(name = "get-videos-retry", fallbackMethod = "getVideosRetry")
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<YouTubeResponseWrapper> getVideoDetails(){
         YouTubeResponseWrapper wrapper = null;
         try{
